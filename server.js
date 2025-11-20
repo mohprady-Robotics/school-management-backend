184d54640a5feaf5a9e5f32ab097e56f3c411879
Pradyumna Mohani <mohprady@gmail.com>
2025-11-20 13:57:33 +0530

    Update CORS configuration

--- server.js
+++ server.js
@@ -30,6 +30,11 @@ app.use((err, req, res, next) => {
     message: err.message || 'Internal Server Error'
   });
 });
+app.use(cors({
+  origin: process.env.FRONTEND_URL || 'http://localhost:3000',
+  credentials: true
+}));
+
 
 const PORT = process.env.PORT || 5000;
 
